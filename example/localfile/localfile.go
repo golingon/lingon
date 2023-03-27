2234f2cba8538c11d91c027e373fba514a1bc82f
Julien Bisconti <veggiemonk@users.noreply.github.com>
2023-03-27 09:11:02 +0200

    Move examples -> example see https://github.com/golang/go/issues/37641

--- example/localfile/localfile.go
+++ example/localfile/localfile.go
@@ -6,7 +6,7 @@ package localfile
 //go:generate go run -mod=readonly github.com/volvo-cars/lingon/cmd/terragen -out ./out -pkg github.com/volvo-cars/lingon/examples/localfile/out -force -provider local=hashicorp/local:2.4.0
 
 import (
-	"github.com/volvo-cars/lingon/examples/localfile/out/providers/local"
+	"github.com/volvo-cars/lingon/example/localfile/out/providers/local"
 	"github.com/volvo-cars/lingon/pkg/terra"
 )
 
