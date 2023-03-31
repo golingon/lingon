80050c5e3188e162c6cd3e2e1ecfaabdfd6b6045
Julien Bisconti <veggiemonk@users.noreply.github.com>
2023-03-31 20:13:00 +0200

    Remove outdated doc

--- example/platypus/pkg/platform/doc.go
+++ example/platypus/pkg/platform/doc.go
@@ -1,13 +1,2 @@
-// Application packaged with Klamydia
-//
-// Example:
-// c := app.NewCiliumApp()
-// if err := kla.Export(c, "cilium"); err != nil {
-// 	panic(err)
-// }
-//
-// if err := cmdexec.Kubectl("apply", "-f", "cilium"); err != nil {
-// 	panic(err)
-// }
-
+// Package platform provides the kubernetes application to build and deploy
 package platform
