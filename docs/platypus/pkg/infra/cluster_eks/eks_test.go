406a09cdc52b286541973a9fa015945f258366cc
Jacob Lärfors <1135394+jlarfors@users.noreply.github.com>
2023-04-03 20:50:47 +0300

    add example test to export and validate a stack

--- docs/platypus/pkg/infra/cluster_eks/eks_test.go
+++ docs/platypus/pkg/infra/cluster_eks/eks_test.go
@@ -6,7 +6,12 @@
 package cluster_eks
 
 import (
+	"context"
+	"os"
+	"path/filepath"
 	"testing"
+
+	"github.com/hashicorp/terraform-exec/tfexec"
 
 	tu "github.com/volvo-cars/lingon/pkg/testutil"
 
@@ -14,6 +19,57 @@ import (
 
 	"github.com/volvo-cars/lingon/pkg/terra"
 )
+
+func testExportValidateStack(
+	t *testing.T, ctx context.Context,
+	stack terra.Exporter,
+) {
+	workdir := filepath.Join(os.TempDir(), t.Name())
+	if err := os.RemoveAll(workdir); err != nil {
+		t.Errorf(
+			"failed removing temporary work dir %s: %s",
+			workdir,
+			err.Error(),
+		)
+		return
+	}
+	if err := os.MkdirAll(workdir, os.ModePerm); err != nil {
+		t.Errorf(
+			"failed creating temporary work dir %s: %s",
+			workdir, err.Error(),
+		)
+	}
+	file, err := os.CreateTemp(workdir, "main_*.tf")
+	if err != nil {
+		t.Errorf(
+			"failed creating temporary file in %s: %s",
+			workdir,
+			err.Error(),
+		)
+		return
+	}
+	defer os.Remove(file.Name()) // clean up
+
+	if err := terra.Export(stack, terra.WithExportWriter(file)); err != nil {
+		t.Errorf(
+			"failed exporting stack to %s: %s",
+			file.Name(),
+			err.Error(),
+		)
+		return
+	}
+	tf, err := tfexec.NewTerraform(workdir, "terraform")
+	tu.AssertNoError(t, err, "creating terraform runtime")
+	if err := tf.Init(ctx); err != nil {
+		tu.AssertNoError(t, err, "initialising terraform config")
+	}
+	tfValidate, err := tf.Validate(ctx)
+	tu.AssertNoError(t, err, "validating terraform config")
+	tu.AssertEqual(t, 0, len(tfValidate.Diagnostics))
+	for _, diag := range tfValidate.Diagnostics {
+		t.Log(diag.Summary)
+	}
+}
 
 func TestEKS(t *testing.T) {
 	type awsStack struct {
@@ -36,6 +92,6 @@ func TestEKS(t *testing.T) {
 		Provider: aws.NewProvider(aws.ProviderArgs{}),
 		Cluster:  *eks,
 	}
-	err := terra.Export(&stack, terra.WithExportOutputDirectory("out"))
-	tu.AssertNoError(t, err, "exporting stack")
+	ctx := context.Background()
+	testExportValidateStack(t, ctx, &stack)
 }
