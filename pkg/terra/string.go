8a565cc8a01a26e1200267fe4da5343ae562fa05
Jacob Lärfors <1135394+jlarfors@users.noreply.github.com>
2024-10-04 13:11:06 +0300

    support string interpolation in terra.StringValue
    resolves #112

--- pkg/terra/string.go
+++ pkg/terra/string.go
@@ -4,9 +4,11 @@
 package terra
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 
+	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/hashicorp/hcl/v2/hclwrite"
 	"github.com/zclconf/go-cty/cty"
 	"github.com/zclconf/go-cty/cty/convert"
@@ -26,6 +28,42 @@ func ReferenceAsString(ref Reference) StringValue {
 		isRef:  true,
 		ref:    ref,
 	}
+}
+
+func mustReferenceTokens(refr Referencer) string {
+	ref, err := refr.InternalRef()
+	if err != nil {
+		panic(
+			fmt.Sprintf(
+				"non-reference value: %s",
+				err.Error(),
+			),
+		)
+	}
+	toks, err := ref.InternalTokens()
+	if err != nil {
+		panic(fmt.Sprintf("getting internal tokens: %s", err.Error()))
+	}
+	return string(toks.Bytes())
+}
+
+// StringFormat can be used for string interpolation.
+// It takes a string with %s placeholders and replaces those with the refs.
+// E.g. given a variable `attr` which references an attribute
+// `resource.id.attr`:
+//
+//	StringFormat("Hello ${%s}", attr) -> "Hello ${resource.id.attr}"
+func StringFormat(s string, refs ...Referencer) StringValue {
+	strRefs := make([]string, len(refs))
+	for i, ref := range refs {
+		strRefs[i] = mustReferenceTokens(ref)
+	}
+	// Slice must of type []interface{} to be used in fmt.Sprintf.
+	anyRefs := make([]interface{}, len(strRefs))
+	for i, ref := range strRefs {
+		anyRefs[i] = ref
+	}
+	return String(fmt.Sprintf(s, anyRefs...))
 }
 
 var _ Value[StringValue] = (*StringValue)(nil)
@@ -72,7 +110,31 @@ func (v StringValue) InternalTokens() (hclwrite.Tokens, error) {
 	if v.isRef {
 		return v.ref.InternalTokens()
 	}
-	return hclwrite.TokensForValue(v.value), nil
+	// We need to support string interpolation, and using
+	// hclwrite.TokensForValue(v.value) will escape `${` to `$${`.
+	// Instead, we marshal the string value to JSON, remove the surrounding
+	// quotes and convert it to hclwrite.Tokens.
+	bStr, err := json.Marshal(v.value.AsString())
+	if err != nil {
+		return nil, fmt.Errorf("marshalling string value: %w", err)
+	}
+	unquotedStr := bStr[1 : len(bStr)-1]
+	toks := hclwrite.Tokens{}
+	toks = append(toks, &hclwrite.Token{
+		Type:  hclsyntax.TokenOQuote,
+		Bytes: []byte{'"'},
+	})
+	if len(unquotedStr) > 0 {
+		toks = append(toks, &hclwrite.Token{
+			Type:  hclsyntax.TokenQuotedLit,
+			Bytes: unquotedStr,
+		})
+	}
+	toks = append(toks, &hclwrite.Token{
+		Type:  hclsyntax.TokenCQuote,
+		Bytes: []byte{'"'},
+	})
+	return toks, nil
 }
 
 func (v StringValue) InternalRef() (Reference, error) {
--- pkg/terra/string_test.go
+++ pkg/terra/string_test.go
@@ -3,10 +3,25 @@
 
 package terra
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func ExampleString() {
 	s := String("hello world")
 	fmt.Println(exampleTokensOrError(s))
 	// 	Output: "hello world"
+}
+
+func ExampleStringFormat() {
+	// Create a dummy resource and create a StringValue reference to it.
+	// Ignore this and pretend you have your reference attribute as you
+	// normally would, e.g.
+	// 	ref.Attributes().Name()
+	res := dummyResource{}
+	ref := ReferenceAsSingle[StringValue](ReferenceResource(&res))
+	// Create a StringValue with a format string and the reference.
+	s := StringFormat("${%s}", ref)
+	fmt.Println(exampleTokensOrError(s))
+	// 	Output: "${dummy.dummy}"
 }
